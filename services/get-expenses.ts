86bd1bf6ec8eb5f9ecc182feb5b452d5b5232bd8
Vinayak Hegde <vinayak.nandi@gmail.com>
2024-12-26 16:06:54 +0000

    feat(expenses): Expense statistics api

---
+++ services/get-expenses.ts
@@ -0,0 +1,12 @@
+import { GET } from "@/app/api/expenses/route";
+import { toCanonical } from "./to-canonical";
+
+export type Expense =  {
+  category: string
+  value: number
+}
+
+export const getExpenses = async () => {
+  const response = await GET();
+  return toCanonical<Expense[]>(await response.json());
+}
