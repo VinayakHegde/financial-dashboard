8aa3453092dca9e3be8319f8e4187c8fcd1ddc4f
Vinayak Hegde <vinayak.nandi@gmail.com>
2024-12-26 10:32:35 +0000

    feat(transaction): transaction api route

---
+++ app/api/transactions/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server'
+import mockTransactions from '@/mock-responses/transactions.json'
+
+export async function GET() {
+  return NextResponse.json(mockTransactions, { status: 200 })
+}
