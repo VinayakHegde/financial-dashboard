354264011cfd183801cfa5db01d967fb3ce63b05
Vinayak Hegde <vinayak.nandi@gmail.com>
2024-12-26 16:07:08 +0000

    feat(expenses): Expense statistics In the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
+import { ExpenseStatistics } from "@/components/expense-statistics";
 import { MyCards } from "@/components/my-cards";
 import { RecentTransactions } from "@/components/recent-transactions";
 import { WeeklyActivity } from "@/components/weekly-activity";
 import { getActiviries } from "@/services/get-activities";
+import { getExpenses } from "@/services/get-expenses";
 import { getMyCards } from "@/services/get-my-cards";
 import { getTransactions } from "@/services/get-transactions";
 
 
 export default async function DashboardPage() {
-  const [cards, transactions, activities] = await Promise.all([
+  const [cards, transactions, activities, expenses] = await Promise.all([
     getMyCards(),
     getTransactions(),
     getActiviries(),
+    getExpenses(),
   ])
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col desktop:flex-row justify-between gap-8">
-        <MyCards cards={cards}/>
+        <MyCards cards={cards} />
         <RecentTransactions transactions={transactions} />
       </div>
 
       <div className="flex flex-col desktop:flex-row justify-between gap-4">
-        <WeeklyActivity activities={activities}/>
-        {/* <ExpenseStatistics /> */}
+        <WeeklyActivity activities={activities} />
+        <ExpenseStatistics expenses={expenses} />
       </div>
       {/*
 
