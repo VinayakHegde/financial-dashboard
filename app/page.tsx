388a627a8e972d5bbaac480cc3fa6013702ebc08
Vinayak Hegde <vinayak.nandi@gmail.com>
2024-12-26 13:17:28 +0000

    feat(activities): weekly activities to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { MyCards } from "@/components/my-cards";
 import { RecentTransactions } from "@/components/recent-transactions";
+import { WeeklyActivity } from "@/components/weekly-activity";
+import { getActiviries } from "@/services/get-activities";
 import { getMyCards } from "@/services/get-my-cards";
 import { getTransactions } from "@/services/get-transactions";
 
 
 export default async function DashboardPage() {
-  const [cards, transactions] = await Promise.all([
+  const [cards, transactions, activities] = await Promise.all([
     getMyCards(),
-    getTransactions()
+    getTransactions(),
+    getActiviries(),
   ])
   return (
     <div className="flex flex-col gap-4">
@@ -16,16 +19,16 @@ export default async function DashboardPage() {
         <RecentTransactions transactions={transactions} />
       </div>
 
-      {/* 
       <div className="flex flex-col desktop:flex-row justify-between gap-4">
-        <WeeklyActivity />
-        <ExpenseStatistics />
+        <WeeklyActivity activities={activities}/>
+        {/* <ExpenseStatistics /> */}
       </div>
+      {/*
 
       <div className="flex flex-col desktop:flex-row justify-between gap-4">
         <QuickTransfer />
         <BalanceHistory />
-      </div> 
+      </div>
       */}
     </div>
   )
